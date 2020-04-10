8f91486909e8d305b0dc8dafb0eaa9f5675a97ea
Hari Dhaka <hari.dhaka@nagarro.com>
2020-04-10 11:00:48 +0530

    Added Todos and Notes table to database

---
+++ database.js
@@ -0,0 +1,53 @@
+const Sequelize = require('sequelize')
+
+const db = new Sequelize({
+    dialect: 'sqlite',
+    storage: __dirname + '/todos.db'
+})
+
+const Todos = db.define('todo', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING(200),
+        allowNull: false,
+    },
+    dueDate: {
+        type: Sequelize.DATE
+    },
+    status:{
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    priority: {
+        type: Sequelize.STRING,
+        defaultValue: "Low"
+    }
+})
+
+const Notes = db.define("notes",{
+    id : {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+    },
+    todoId: {
+        type: Sequelize.INTEGER,
+        allowNull:false
+    },
+    value: {
+        type: Sequelize.STRING(300),
+        allowNull: false
+    }
+})
+
+module.exports = {
+    db, Todos , Notes
+}
